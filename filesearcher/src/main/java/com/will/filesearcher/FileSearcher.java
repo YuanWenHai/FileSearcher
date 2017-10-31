4a67d94e97cb38c1b6035df5bc77cd5c986c9352
YuanWenHai <a2265839@gmail.com>
2017-10-31 16:44:26 +0800

    重写FileSearcher工具.

---
+++ filesearcher/src/main/java/com/will/filesearcher/FileSearcher.java
@@ -0,0 +1,8 @@
+package com.will.filesearcher;
+
+/**
+ * Created by Will on 2017/10/31.
+ */
+
+public class FileSearcher  {
+}
---
+++ filesearcher/src/main/java/com/will/filesearcher/FileSearcherActivity.java
@@ -0,0 +1,17 @@
+package com.will.filesearcher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Will on 2017/10/31.
+ */
+
+public class FileSearcherActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
